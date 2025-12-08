0082ff965c8b1f7ca4c42f1a67f0263d6b2e952f
uzumaki-ak <anikydv38@gmail.com>
2025-12-08 10:25:03 +0530

    done with / route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Reenie_Beanie, VT323 } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const reenieBeanie = Reenie_Beanie({
+  weight: "400",
+  variable: "--font-reenie-beanie",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const vt323 = VT323({
+  weight: "400",
+  variable: "--font-vt323", 
   subsets: ["latin"],
 });
 
@@ -19,14 +21,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${reenieBeanie.variable} ${vt323.variable} antialiased`}>
         {children}
       </body>
     </html>
